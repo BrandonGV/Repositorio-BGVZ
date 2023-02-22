20ebdd04354ab993f0a5026e3c5df53d6c36419c
BrandonGV <brandon-gvs@outlook.es>
2023-02-22 15:51:55 -0500

    operaciones artitmeticas v2

--- Algoritmos/operadoresaritmeticos.py
+++ Algoritmos/operadoresaritmeticos.py
@@ -1,10 +1,23 @@
+#libreria
+import math
 #addiquirir informacion
 a = int (input("digite el numero 1:"))
 print(f"el numero a es:{a}")
 b = int (input("digite el numero 2:"))
 print(f"el numero a es:{b}")
+
 #proceso
 c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a ** b
+h = math.sqrt(a)
 
 #salida
 print(f"el resultado de la suma es : {c}")
+print(f"el resultado de la resta es : {d}")
+print(f"el resultado de la multiplicación es : {e}")
+print(f"el resultado de la división es : {f}")
+print(f"el resultado de la potencia es : {g}")
+print(f"el resultado de la raiz de a es : {h}")
