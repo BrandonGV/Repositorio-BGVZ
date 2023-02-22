85e3056dcd0e65f78b8f308ef1f21b50a3f00e2b
BrandonGV <brandon-gvs@outlook.es>
2023-02-22 15:24:11 -0500

    suma

---
+++ Algoritmos/operadoresaritmeticos.py
@@ -0,0 +1,10 @@
+#addiquirir informacion
+a = int (input("digite el numero 1:"))
+print(f"el numero a es:{a}")
+b = int (input("digite el numero 2:"))
+print(f"el numero a es:{b}")
+#proceso
+c = a + b
+
+#salida
+print(f"el resultado de la suma es : {c}")
