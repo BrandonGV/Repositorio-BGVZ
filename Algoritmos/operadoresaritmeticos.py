e96ce8b4d845a033f0d611b313a069887877fa31
BrandonGV <brandon-gvs@outlook.es>
2023-03-01 13:36:28 -0500

    Ejercicio 2

--- Algoritmos/operadoresaritmeticos.py
+++ Algoritmos/operadoresaritmeticos.py
@@ -2,6 +2,13 @@
 #libreria
 
 import math
+#PRIMER EJERCICIO
+operador = ((15/3)*(7+(68-15*33+((45**2)/5)/3)/2)+19)
+print(float(operador))
+#SEGUNDO EJERCICIO
+
+operador2 = (4+8*2)/4-3**2+math.sqrt(4)
+print(float(operador2))
 #addiquirir informacion
 a = int (input("digite el numero 1:"))
 print(f"el numero a es:{a}")
@@ -23,6 +30,3 @@ print(f"el resultado de la multiplicación es : {e}")
 print(f"el resultado de la división es : {f}")
 print(f"el resultado de la potencia es : {g}")
 print(f"el resultado de la raiz de a es : {h}")
-
-operador = ((15/3)*(7+(68-15*33+((45**2)/5)/3)/2)+19)
-print(float(operador))
