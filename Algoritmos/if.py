48e058881905255a60142cf009027fefe6c004e9
BrandonGV <brandon-gvs@outlook.es>
2023-03-01 14:38:33 -0500

    IF

---
+++ Algoritmos/if.py
@@ -0,0 +1,33 @@
+
+precio =501
+#Estructura if
+if precio>=500:
+    print("esta muy caro")
+#Estructura if -else
+if precio>=500:
+    print("esta muy caro")
+else:
+    print("lo compro")
+#estructura if anidada
+if precio<=500:
+    print("esta muy caro")
+elif precio == 501: 
+    print(precio+100)
+else:
+    print("lo compro")
+
+#Ejemplo if
+numero = int(input("digite un numero del 1 al 5"))
+if numero==1:
+    print("el numero que digito es 1")
+elif numero==2:
+    print("el numero que digito es 2")
+elif numero==3:
+    print("el numero que digito es 3")
+elif numero==4:
+    print("el numero que digito es 4")
+elif numero==5:
+    print("el numero que digito es 5")
+else:
+    print("error")
+
