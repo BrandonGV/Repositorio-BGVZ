3bf8a3e03aa243c3bce0fcd3ad3ee84a3caa2c54
BrandonGV <brandon-gvs@outlook.es>
2023-03-01 13:54:21 -0500

    Calculadora bases numericas

---
+++ Algoritmos/Sistemas_numericos.py
@@ -0,0 +1,10 @@
+numero = int(input("digite el numero a transformar: "))
+#conv. decimal binario
+binario = bin(numero)
+print(f"binario{binario}")
+#conv. decimal hexa
+hexa = hex(numero)
+print(f"hexadecimal{hexa}")
+#conv. decimal-octal
+octal = oct(numero)
+print(f"octal{octal}")
