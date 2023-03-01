22de25fab13cd2660fee2c684fd34180e7d1d7d2
BrandonGV <brandon-gvs@outlook.es>
2023-03-01 14:21:39 -0500

    espacios

--- Algoritmos/operadoresaritmeticos.py
+++ Algoritmos/operadoresaritmeticos.py
@@ -2,13 +2,14 @@
 #libreria
 
 import math
+"""""
 #PRIMER EJERCICIO
 operador = ((15/3)*(7+(68-15*33+((45**2)/5)/3)/2)+19)
 print(float(operador))
 #SEGUNDO EJERCICIO
-
 operador2 = (4+8*2)/4-3**2+math.sqrt(4)
 print(float(operador2))
+"""""
 #addiquirir informacion
 a = int (input("digite el numero 1:"))
 print(f"el numero a es:{a}")
