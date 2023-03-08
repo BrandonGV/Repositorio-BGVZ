11dd0cd3c408b5abfd0acf3dae2a125ee3e4532f
BrandonGV <brandon-gvs@outlook.es>
2023-03-08 14:05:28 -0500

    While y break implementados

---
+++ Algoritmos/while.py
@@ -0,0 +1,13 @@
+#Variable
+contador = 0
+
+#Estructura
+while contador <= 4:
+    if contador == 3:
+        print("unisangil")
+        break
+    print(f"contador = {contador}")
+    contador += 1
+print("salio del while")
+
+#Break y continue
