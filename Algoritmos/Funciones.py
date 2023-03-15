da9a7d9fce8943ff5172fb691307e2eafb47fec6
BrandonGV <brandon-gvs@outlook.es>
2023-03-15 15:17:31 -0500

    funciones

---
+++ Algoritmos/Funciones.py
@@ -0,0 +1,14 @@
+#Funciones
+"""""
+def mensaje():
+    print("Unisangil")
+    """""
+def suma (a,b):
+    c= a+b
+    print(c)
+suma(1,2)
+#parametros valores de retorno
+def suma_resta(a,b):
+    d =a+b
+    e =a-b
+    return
