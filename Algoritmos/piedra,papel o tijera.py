ee6c14ae9bb944555a98e5867c87c1f25291b2b0
BrandonGV <brandon-gvs@outlook.es>
2023-03-14 22:03:46 -0500

    Se implementaron las rondas y dictar el ganador.

--- Algoritmos/piedra,papel o tijera.py
+++ Algoritmos/piedra,papel o tijera.py
@@ -1,40 +1,50 @@
-#importar libreria
+#Importar libreria
 import random
-#opciones
-op =("Piedra","Papel","Tijera")
-#entradas
-usuario = input("Digite la opción piedra, papel o tijera:  ")
-cpu = random.choice(op)
-#Imprimir informacion
-print(f"la opción que digito el usuario es {usuario}")
-print(f"la opcion de la cpu es : {cpu}")
-#Proceso
-#empate
-if usuario == "piedra" and cpu == "piedra":
-    print("Empate")
-elif usuario == "Tijera" and cpu == "Tijera":
-    print("Empate")
-elif usuario == "Papel" and cpu == "Papel":
-    print("Empate")
-#gana usuario
-elif usuario == "Piedra" and cpu == "Tijera":
-    print("Gana usuario")
-elif usuario == "Papel" and cpu == "Piedra":
-    print("Gana usuario")
-elif usuario == "Tijera" and cpu == "Papel":
-    print("Gana usuario")
-elif usuario == "Papel" and cpu == "Tijera":
-    print("Gana cpu")
-
-#gana cpu
-elif cpu == "Piedra" and usuario == "Tijera":
-    print("Gana cpu")
-elif cpu == "Papel" and usuario == "Piedra":
-    print("Gana cpu")
-elif cpu == "Tijera" and usuario == "Papel":
-    print("Gana cpu")
-elif cpu == "Papel" and usuario == "Tijera":
-    print("Gana usuario")
-
-else:
-    print("Error :v")
+#Opciones
+contador1=int(input("cuantas rondas quieres jugar: "))
+op = ("Piedra","Papel","Tijera")
+contador=1
+puntuacion_usuario=0
+puntuacion_cpu=0
+while contador<=contador1:
+    #Entradas
+    contador += 1
+    usuario = input("Digite la opción Pierdra, Papel o Tijera:  ")
+    cpu = random.choice(op)
+    #Imprimir informacion
+    print(f"La op que digito el usuario es: {usuario}")
+    print(f"La op CPU es: {cpu}")
+    #Proceso
+    if usuario == "Piedra" and cpu == "Piedra":
+        print("Empate!!!")
+    elif usuario == "Tijera" and cpu == "Tijera":
+        print("Empate!!!")
+    elif usuario == "Papel" and cpu == "Papel":
+        print("Empate!!!")
+    elif usuario == "Piedra" and cpu == "Papel":
+        print("Gana cpu!!!")
+        puntuacion_cpu +=1
+    elif usuario == "Piedra" and cpu == "Tijera":
+        print("Gana usuario!!!")
+        puntuacion_usuario +=1
+    elif usuario == "Papel" and cpu == "Piedra":
+        print("Gana usuario!!!")
+        puntuacion_usuario +=1
+    elif usuario == "Papel" and cpu == "Tijera":
+        print("Gana cpu!!!")
+        puntuacion_cpu +=1
+    elif usuario == "Tijera" and cpu == "Piedra":
+        print("Gana cpu!!!")
+        puntuacion_cpu +=1
+    elif usuario == "Tijera" and cpu == "Papel":
+        print("Gana usuario!!!")
+        puntuacion_usuario +=1
+    else:
+        print("Error!!!")
+        contador -=1
+if puntuacion_usuario>puntuacion_cpu:
+    print(f"gana el usuario con {puntuacion_usuario} puntos y perdio la cpu con {puntuacion_cpu} puntos")
+elif puntuacion_usuario<puntuacion_cpu:
+    print(f"gana la cpu con {puntuacion_cpu} puntos y perdiste con {puntuacion_usuario} puntos")
+elif puntuacion_cpu==puntuacion_usuario:
+    print(f"fue un empate la cpu gano {puntuacion_cpu} y tu ganaste {puntuacion_usuario} ¨")
